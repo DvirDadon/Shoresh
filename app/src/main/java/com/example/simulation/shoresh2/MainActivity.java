8e8a21f71aae10dc81be088296fe807e147cfca7
DvirDadon <dd2640@bs.amalnet.k12.il>
2019-04-24 00:17:12 +0300

    Initial commit

---
+++ app/src/main/java/com/example/simulation/shoresh2/MainActivity.java
@@ -0,0 +1,80 @@
+package com.example.simulation.shoresh2;
+
+import android.content.Intent;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import java.util.Random;
+
+public class MainActivity extends AppCompatActivity {
+    Double a, b, c,X1,X2;
+    String aa, bb, cc;
+    Random rnd = new Random();
+    EditText A;
+    EditText B;
+    EditText C;
+    TextView answer1;
+    TextView answer2;
+
+
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        A = (EditText) findViewById(R.id.A);
+        B = (EditText) findViewById(R.id.B);
+        C = (EditText) findViewById(R.id.C);
+        answer1 = (TextView) findViewById(R.id.answer1);
+        answer2 = (TextView) findViewById(R.id.answer2);
+        }
+
+
+
+        public void Random (View view){
+            a = rnd.nextInt(100) + 1 + rnd.nextDouble();
+            b = rnd.nextInt(100) + 1 + rnd.nextDouble();
+            c = rnd.nextInt(100) + 1 + rnd.nextDouble();
+            A.setText("" + a);
+            B.setText("" + b);
+            C.setText("" + c);
+        }
+
+
+        public void Solution (View view){
+            Intent si = new Intent(this, Pitaron.class);
+            aa = A.getText().toString();
+            bb = B.getText().toString();
+            cc = C.getText().toString();
+            if((aa.equals(""))||(bb.equals(""))||(cc.equals(""))||(aa.equals("-"))||(bb.equals("-"))||(cc.equals("-")))
+                Toast.makeText(this, "Enter ax^2+bx+c before doing a solution", Toast.LENGTH_SHORT).show();
+            else {
+                a = Double.parseDouble(aa);
+                b = Double.parseDouble(bb);
+                c = Double.parseDouble(cc);
+                si.putExtra("n", a);
+                si.putExtra("nn", b);
+                si.putExtra("nnn", c);
+                startActivityForResult(si, 1);
+
+            }
+        }
+        @Override
+        protected void onActivityResult(int source , int good, @Nullable Intent data){
+        if(source==1){
+            if (good==RESULT_OK){
+                if (data!=null) {
+                    X1=data.getDoubleExtra("n",9999.9);
+                    answer1.setText("x1="+" "+ X1);
+                    answer2.setText("x2=" +" "+ data.getDoubleExtra("nn", 9999.9));
+                }
+                }
+        }
+        }
+    }
---
+++ app/src/main/java/com/example/simulation/shoresh2/Pitaron.java
@@ -0,0 +1,62 @@
+package com.example.simulation.shoresh2;
+
+import android.content.Intent;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+import android.widget.TextView;
+
+public class Pitaron extends AppCompatActivity {
+    TextView x1,x2;
+    double a,b,c,d,e,f;
+    WebView Wv;
+    String url,aa,bb,cc;
+    Intent gi;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_pitaron);
+        gi = getIntent();
+        a=gi.getDoubleExtra("n",999.9);
+        b=gi.getDoubleExtra("nn",999.9);
+        c=gi.getDoubleExtra("nnn",999.9);
+        if ((a!=999.9) || (b!=999.9)||(c!=999.9)) {
+            x1 = (TextView) findViewById(R.id.x1);
+            x2 = (TextView) findViewById(R.id.x2);
+            Wv = (WebView) findViewById(R.id.wV);
+            url = "https://www.google.com/search?ei=mkSmXM6QIeOS1fAP_aaKwAk&q=" + a + "x%5E2%2B" + b + "x%2B" + c + "&oq=5x%5E2%2B2x%2B3&gs_l=psy-ab.3..0i203j0i30l9.7136711.7143291..7143563...0.0..0.151.1143.0j9......0....1..gws-wiz.......35i39j0i131j0j35i39i19.WDPr0vE0iME";
+            b = b * (-1);
+            d = Math.sqrt(Math.pow(b, 2) + (-4 * a * c));
+            a = a * 2;
+            e = (b + d) / a;
+            f = (b - d) / a;
+            x1.setText("x1="+" "+ e);
+            x2.setText("x2="+" "+ f);
+            Wv.getSettings().setJavaScriptEnabled(true);
+            Wv.loadUrl(url);
+            Wv.setWebViewClient(new WebViewClient());
+        }
+    }
+
+
+
+    public void Kelet(View view) {
+        gi.putExtra("n",e);
+        gi.putExtra("nn",f);
+        setResult(RESULT_OK,gi);
+        finish();
+    }
+
+    private class MyWebViewClient extends WebViewClient {
+    @Override
+        public boolean shouldOverrideUrlLoading(WebView view ,String url){
+            view.loadUrl(url);
+            return true;
+        }
+    }
+
+
+}
